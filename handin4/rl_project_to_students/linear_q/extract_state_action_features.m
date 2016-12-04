0697bf1ca4064a2a7d3f03310cd977bb86756ee3
Edvard Johansson <edvard.jo@gmail.com>
2016-12-04 10:24:40 +0100

    working soso

---
+++ handin4/rl_project_to_students/linear_q/extract_state_action_features.m
@@ -0,0 +1,235 @@
+function [state_action_feats, prev_grid, prev_head_loc] = extract_state_action_features(prev_grid, grid, prev_head_loc, nbr_feats)
+%
+% Code may be changed in this function, but only where it states that it is 
+% allowed to do so
+%
+% Code part of ML-2016
+%
+% Function to extract state-action features, based on current and previous
+% grids (game screens)
+%
+% Input:
+%
+% prev_grid     - Previous grid (game screen), N-by-N matrix. If initial
+%                 time-step: prev_grid = grid, else: prev_grid != grid.
+% grid          - Current grid (game screen), N-by-N matrix. If initial
+%                 time-step: prev_grid = grid, else: prev_grid != grid.
+% prev_head_loc - The previous location of the head of the snake (from the 
+%                 previous time-step). If initial time-step: Assumed known,
+%                 else: inferred in function "update_snake_grid.m" (so in
+%                 practice it will always be known in this function)
+% nbr_feats     - Number of state-action features per action. Set this 
+%                 value appropriately in the calling script "snake.m", to
+%                 match the number of state-action features per action you
+%                 end up using
+%
+% Output:
+%
+% state_action_feats - nbr_feats-by-|A| matrix, where |A| = number of
+%                      possible actions (|A| = 3 in Snake), and nbr_feats
+%                      is described under "Input" above. This matrix
+%                      represents the state-action features extracted given
+%                      the current and previous grids (game screens)
+% prev_grid          - The previous grid as seen from one step in the
+%                      future, i.e., prev_grid is set to the input grid
+% prev_head_loc      - The previous head location as seen from one step
+%                      in the future, i.e., prev_head_loc is set to the
+%                      current head location (the current head location is
+%                      inferred in the code below)
+%
+% Bugs, ideas etcetera: send them to the course email
+
+% Extract grid size
+N = size(grid, 1);
+
+% Initialize state_action_feats to nbr_feats-by-3 matrix
+state_action_feats = nan(nbr_feats, 3);
+
+% Based on how grid looks now and at previous time step, infer head location
+change_grid = grid - prev_grid;
+prev_grid   = grid; % Used in later calls to "extract_state_action_features.m"
+
+% Find head location (initially known that it is in center of grid)
+if nnz(change_grid) > 0 % True, except in initial time-step
+    [head_loc_m, head_loc_n] = find(change_grid > 0);
+else % True only in initial time-step
+    head_loc_m = round(N / 2);
+    head_loc_n = round(N / 2);
+end
+head_loc = [head_loc_m, head_loc_n];
+
+% Previous head location
+prev_head_loc_m = prev_head_loc(1);
+prev_head_loc_n = prev_head_loc(2);
+
+% Infer current movement directory (N/E/S/W) by looking at how current and previous
+% head locations are related
+if prev_head_loc_m == head_loc_m + 1 && prev_head_loc_n == head_loc_n     % NORTH
+    movement_dir = 1;
+elseif prev_head_loc_m == head_loc_m && prev_head_loc_n == head_loc_n - 1 % EAST
+    movement_dir = 2;
+elseif prev_head_loc_m == head_loc_m - 1 && prev_head_loc_n == head_loc_n % SOUTH
+    movement_dir = 3;
+else                                                                      % WEST
+    movement_dir = 4;
+end
+
+% The current head_loc will at the next time-step be prev_head_loc
+prev_head_loc = head_loc;
+
+% HERE BEGINS YOUR STATE-ACTION FEATURE ENGINEERING. ALL CODE BELOW IS 
+% ALLOWED TO BE CHANGED IN ACCORDANCE WITH YOUR CHOSEN FEATURES. 
+% Some skeleton code is provided to help you get started. Also, have a 
+% look at the function "get_next_info" (see bottom of this function).
+% You may find it useful.
+
+[apple_loc_m, apple_loc_n] = find(grid == -1);
+apple_loc = [apple_loc_m, apple_loc_n];
+
+feature_index = 1;
+for action = 1 : 3 % Evaluate all the different actions (left, forward, right)
+    
+    % Feel free to uncomment below line of code if you find it useful
+    [next_head_loc, next_move_dir] = get_next_info(action, movement_dir, head_loc);
+    
+    % Replace this to fit the number of state-action features per features
+    % you choose (3 are used below), and of course replace the randn() 
+    % by something more sensible
+    current_apple_vector = head_loc - apple_loc;
+    next_apple_vector = next_head_loc - apple_loc;
+    
+    current_1_norm_apple_dist = sum(abs(current_apple_vector));
+    next_1_norm_apple_dist = sum(abs(next_apple_vector));
+    
+%     current_2_norm = sum(current_apple_vector.^2);
+%     next_2_norm = sum(next_apple_vector.^2);
+%     
+%     current_inf_norm = max(abs(current_apple_vector));
+%     next_inf_norm = max(abs(next_apple_vector));
+    %state_action_feats(feature_index, action) = current_1_norm_apple_dist - next_1_norm_apple_dist;
+    
+    state_action_feats(feature_index, action) = next_1_norm_apple_dist-current_1_norm_apple_dist;
+    feature_index = feature_index + 1;
+    
+    
+    
+%    state_action_feats(feature_index, action) = current_2_norm - next_2_norm;
+ %   feature_index = feature_index + 1;
+    
+ %   state_action_feats(feature_index, action) = current_inf_norm - next_inf_norm;
+ %   feature_index = feature_index + 1;
+    
+    %Change in number of number of 4-neighbour bwlabels
+    [current_l, current_NUM] = bwlabel(grid~=1, 4);
+    
+    h = hist(current_l(:), 0:current_NUM);
+    h = h(2:end);
+    [~,best_island] = max(h);
+    is_good_island = current_l(next_head_loc(1), next_head_loc(2))==best_island;
+  
+    %state_action_feats(feature_index, action) = is_good_island*2-1;
+    %feature_index = feature_index+1;  
+    
+    %     if ~is_good_island
+%         disp('for testing purposes')
+%     end
+%         
+    
+    next_grid = grid;
+    %don't care about the last part of the tail being left, sometimes eats
+    %apple
+    next_grid(next_head_loc(1), next_head_loc(2)) = 1;
+    [next_l, next_NUM] = bwlabel(next_grid==0, 4);
+    
+    %state_action_feats(feature_index, action) = current_NUM-next_NUM;
+    %feature_index = feature_index+1;
+    if (next_NUM > 1)
+        %disp('blockade')
+        t = 1;
+    end
+    
+    %Check if will go inside smallest bwlabel
+    
+    %Deadly move?
+    grid_value_next_head_loc = grid(next_head_loc(1), next_head_loc(2));
+    state_action_feats(feature_index, action) = -grid_value_next_head_loc;
+    feature_index = feature_index + 1;
+    
+%    state_action_feats(3, action) = randn();
+    feature_index = 1;
+end
+if any(state_action_feats(2,:) == -1)
+    state_action_feats
+end
+if any(isnan(state_action_feats))
+    t=0;
+end
+%state_action_feats
+end
+
+function [next_head_loc, next_move_dir] = get_next_info(action, movement_dir, head_loc)
+% Function to infer next haed location and movement direction
+
+% Extract relevant stuff
+head_loc_m = head_loc(1);
+head_loc_n = head_loc(2);
+
+if movement_dir == 1 % NORTH
+    if action == 1     % left
+        next_head_loc_m = head_loc_m;
+        next_head_loc_n = head_loc_n - 1;
+        next_move_dir   = 4; 
+    elseif action == 2 % forward
+        next_head_loc_m = head_loc_m - 1;
+        next_head_loc_n = head_loc_n;
+        next_move_dir   = 1;
+    else               % right
+        next_head_loc_m = head_loc_m;
+        next_head_loc_n = head_loc_n + 1;
+        next_move_dir   = 2;
+    end
+elseif movement_dir == 2 % EAST
+    if action == 1
+        next_head_loc_m = head_loc_m - 1;
+        next_head_loc_n = head_loc_n;
+        next_move_dir   = 1;
+    elseif action == 2
+        next_head_loc_m = head_loc_m;
+        next_head_loc_n = head_loc_n + 1;
+        next_move_dir   = 2;
+    else
+        next_head_loc_m = head_loc_m + 1;
+        next_head_loc_n = head_loc_n;
+        next_move_dir   = 3;
+    end
+elseif movement_dir == 3 % SOUTH
+    if action == 1
+        next_head_loc_m = head_loc_m;
+        next_head_loc_n = head_loc_n + 1;
+        next_move_dir   = 2;
+    elseif action == 2
+        next_head_loc_m = head_loc_m + 1;
+        next_head_loc_n = head_loc_n;
+        next_move_dir   = 3;
+    else
+        next_head_loc_m = head_loc_m;
+        next_head_loc_n = head_loc_n - 1;
+        next_move_dir   = 4;
+    end
+else % WEST
+    if action == 1
+        next_head_loc_m = head_loc_m + 1;
+        next_head_loc_n = head_loc_n;
+        next_move_dir   = 3;
+    elseif action == 2
+        next_head_loc_m = head_loc_m;
+        next_head_loc_n = head_loc_n - 1;
+        next_move_dir   = 4;
+    else
+        next_head_loc_m = head_loc_m - 1;
+        next_head_loc_n = head_loc_n;
+        next_move_dir   = 1;
+    end
+end
+next_head_loc = [next_head_loc_m, next_head_loc_n];
+end
---
+++ handin4/rl_project_to_students/linear_q/snake.m
@@ -0,0 +1,201 @@
+% Code may be changed in this script, but only where it states that it is allowed 
+% to do so
+%
+% To be clear, you may change any code in this entire project, but for the 
+% hand-in, you are to use the code as it is, and only change some code in
+% valid places of this script (and some other functions). So if you feel 
+% experimental, which hopefully you do, download two sets of the code 
+% and play around with one part as you wish, and then use the original code
+% (with some valid and necessary changes) in the hand-in
+%
+% Code part of ML-2016
+%
+% This script runs the game Snake. There is no possibility to play the game
+% oneself, but it is possible to train (Q-learning with linear function 
+% approximation) a policy used by an agent that can then play the game
+%
+% SEE extract_state_action_features.m IN WHICH YOU NEED TO ENGINEER 
+% STATE-ACTION FEATURES (SEE EXERCISE 8)
+%
+% Bugs, ideas etcetera: send them to the course email
+
+% Begin with a clean sheet
+clc;
+close all;
+clearvars;
+
+% Ensure same randomization process (repeatability)
+rng(5);
+
+% Define size of the snake grid (N-by-N)
+N = 30;
+
+% Define length of initial snake (will be placed at center, pointing in
+% random direction (north/east/south/west))
+snake_len_init = 10;
+
+% Define initial number of apples (placed at random locations, currently only tested with 1 apple)
+nbr_apples = 1;
+
+% Updates per second
+updates_per_sec = 20;                  % Allowed to be changed (though your code must handle 20 updates per second at the lowest)
+pause_time      = 1 / updates_per_sec; % DO NOT CHANGE
+
+% Set visualization settings (what you as programmer will see when the agent is playing)
+show_fraction  = 1/50;                        % Allowed to be changed. 1: show everything, 0: show nothing, 0.1: show every tenth, and so on...
+show_every_kth = round(1 / show_fraction); % DO NOT CHANGE
+
+% Stuff related to learning agent (YOU SHOULD EXPERIMENT A LOT WITH THESE
+% SETTINGS - SEE EXERCISE 8)
+nbr_feats          = 2;                                             % Number of state-action features per action
+nbr_ep             = 300;                                          % Number of episodes (full games until snake dies) to train
+rewards            = struct('default', 0, 'apple', 10, 'death', -10); % Experiment with different reward signals, to see which yield a good behaviour for the agent
+gamm               = 0.9;                                           % Discount factor in Q-learning
+alph               = 0.9;                                          % Learning rate in Q-learning
+eps                = 0.05;                                          % Random action selection probability in epsilon-greedy Q-learning (lower: increase exploitation, higher: increase exploration)
+alph_update_iter   = 100;                                             % 0: Never update alpha, Positive integer k: Update alpha every kth episode
+alph_update_factor = 0.9;                                           % At alpha update: new alpha = old alpha * alph_update_factor
+eps_update_iter    = 10;                                             % 0: Never update eps, Positive integer k: Update eps every kth episode
+eps_update_factor  = 0.99;                                           % At eps update: new eps = old eps * eps_update_factor
+weights            = randn(nbr_feats, 1);                           % I.i.d. N(0,1) initial weights
+
+% Below two commands are useful when you have tranined your agent and later
+% want to test it (see also Exercise 8). Remember to set alph = eps = 0 in 
+% testing!
+
+% save('weights.mat', 'weights');
+% load weights;alph = 1; eps = 0;show_fraction  =1; show_every_kth = round(1 / show_fraction);  
+% save('Q_eps_alph', 'Q_vals', 'eps', 'alph');
+%load Q_eps_alph; alph = 0; eps = 0;show_fraction  =1; show_every_kth = round(1 / show_fraction);
+
+
+
+% Keep track of high score, minimum score and store all scores 
+top_score  = 0;
+min_score  = 500;
+all_scores = nan(1, nbr_ep);
+
+% This is the main loop for running the agent and/or learning process to play the game
+for i = 1 : nbr_ep
+    
+    % Display what episode we're at and current weights
+    disp(['EPISODE: ', num2str(i)]);
+    disp('WEIGHTS: ')
+    disp(weights)
+    
+    % Check if learning rate and/or eps should decrease
+    if rem(i, alph_update_iter) == 0
+        disp('LOWERING ALPH!');
+        alph = alph_update_factor * alph %#ok<*NOPTS>
+    end
+    if rem(i, eps_update_iter) == 0
+        disp('LOWERING EPS!');
+        eps = eps_update_factor * eps
+    end
+    
+    % Generate initial snake grid and possibly show it
+    close all;
+    snake_len                          = snake_len_init;
+    [grid, head_loc]                   = gen_snake_grid(N, snake_len, nbr_apples); % Get initial stuff
+    score                              = 0;                                        % Initial score: 0
+    [prev_head_loc_m, prev_head_loc_n] = find(grid == snake_len);                  % Used in updates
+    prev_head_loc                      = [prev_head_loc_m, prev_head_loc_n];       % Used by player/agent
+    prev_head_loc_agent                = prev_head_loc;                            % Agent also knows "previous" head location initially
+    grid_show                          = grid;                                     % What is shown on screen is different from what exact is happening "under the hood"
+    grid_show(grid_show > 0)           = 1;                                        % This is what is seen by the player
+    prev_grid_show                     = grid_show;                                % Used by agent - needs at least two frames to keep track of head location
+    if rem(i, show_every_kth) == 0
+        figure; imagesc(grid_show)
+    end
+
+    % Initialize time-step
+    t = 0;
+    
+    % This while-loop runs until the snake dies and the game ends
+    while 1
+        
+        % Run epsilon-greedy Q-learning
+
+        % Extract state-action features and update relevant stuff (prev_grid_show, prev_head_loc)
+        % Note that once we begin computing TD(1)-errors, we make a one-step lookahead; therefore, 
+        % for t > 0, we can simply copy the relevant things computed during the lookahead
+        if t == 0
+            [state_action_feats, prev_grid_show, prev_head_loc_agent] = extract_state_action_features(prev_grid_show, grid_show, prev_head_loc_agent, nbr_feats);
+        else
+            state_action_feats  = state_action_feats_future;
+            prev_grid_show      = prev_grid_show_future;
+            prev_head_loc_agent = prev_head_loc_agent_future; 
+        end
+
+        % epsilon-greedy action selection
+        if rand < eps % Select random action
+            action = randi(3);
+        else % Select greedy action (maximizing Q(s,a))
+            Q_vals      = Q_fun(weights, state_action_feats);
+            [~, action] = max(Q_vals);
+        end
+        
+        % Possibly pause for a while
+        if rem(i, show_every_kth) == 0
+            pause(pause_time);
+        end
+        
+        % Update state and obtain reward 
+        [grid, head_loc, prev_head_loc, snake_len, score, reward, terminate] = update_snake_grid(grid, head_loc, prev_head_loc, snake_len, score, rewards, action);
+        
+        % Check for termination
+        if terminate
+            
+            % Compute terminal TD(1)-error
+            target = reward; % No one-step lookahead here - simply look at the reward
+            pred   = Q_fun(weights, state_action_feats, action); 
+            td_err = target - pred;
+
+            % Update weights based on TD(1)-error
+            weights = weights + alph * td_err * state_action_feats(:, action);
+            
+            % Insert score into container
+            all_scores(i) = score;
+            
+            % Display stuff
+            disp(['GAME OVER! SCORE:       ', num2str(score)]);
+            disp(['AVERAGE SCORE SO FAR:   ', num2str(mean(all_scores(1 : i)))]);
+            if i >= 10
+                disp(['AVERAGE SCORE LAST 10:  ', num2str(mean(all_scores(i - 9 : i)))]);
+            end
+            if i >= 100
+                disp(['AVERAGE SCORE LAST 100: ', num2str(mean(all_scores(i - 99 : i)))]);
+            end
+            if score > top_score
+            disp(['NEW HIGH SCORE!         ', num2str(score)]);
+                top_score = score;
+            end
+            if score < min_score
+            disp(['NEW SMALLEST SCORE!     ', num2str(score)]);
+                min_score = score;
+            end
+            
+            % Terminate
+            break;
+        end
+        
+        % Update what to show to the agent (and possibly programmer)
+        grid_show                = grid;
+        grid_show(grid_show > 0) = 1;
+        if rem(i, show_every_kth) == 0
+            imagesc(grid_show);
+        end
+        
+        % Compute TD(1)-error
+        [state_action_feats_future, prev_grid_show_future, prev_head_loc_agent_future] = extract_state_action_features(prev_grid_show, grid_show, prev_head_loc_agent, nbr_feats);
+        target                                                                         = reward + gamm * max(Q_fun(weights, state_action_feats_future));
+        pred                                                                           = Q_fun(weights, state_action_feats, action);
+        td_err                                                                         = target - pred;
+
+        % Update weights based on TD(1)-error
+        weights = weights + alph * td_err * state_action_feats(:, action);
+        
+        % Update time-step
+        t = t + 1;
+    end
+end
